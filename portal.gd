052d3e9476ebe7d3c9341a41e187edac613b453a
Lucas Fryzek <lucas.fryzek@fryzekconcepts.com>
2025-01-25 10:52:58 +0000

    Añadir portal basico

---
+++ portal.gd
@@ -0,0 +1,21 @@
+extends Area3D
+
+@export var destino: Node3D
+@export var portal_cooldown: float = 0.5
+
+var puede_portal = true
+
+func _ready() -> void:
+	assert(destino != null)
+
+func _on_body_entered(body: Node3D) -> void:
+	if puede_portal:
+		destino.empieza_cooldown()
+		body.position = destino.position
+	
+func empieza_cooldown() -> void:
+	puede_portal = false
+	$Timer.start(portal_cooldown)
+
+func _on_timer_timeout() -> void:
+	puede_portal = true
