422a2cfed5750e38a6f68834d126d19eb5fd8248
danielmarh <danielmarh333@gmail.com>
2025-01-25 15:27:29 +0000

    Main menu interactuar
    Al pulsar la E en el placeholder empieza el juego

--- scripts/Empezar.gd
+++ scripts/Empezar.gd
@@ -1,8 +1,19 @@
 extends StaticBody3D
 
-var puede_interactuar = false
-	
+var puede_interactuar
+
+func _ready() -> void:
+	puede_interactuar = false
+
+func _on_body_exited(body: Node3D) ->void:
+	print("Jugador sale")
+	puede_interactuar = false
+
 func _on_body_entered(body: Node3D) -> void:
 	print("Jugador collision")
 	puede_interactuar = true
-	
+
+func _input(ev):
+	if Input.is_action_pressed("Interactuar"):
+		if puede_interactuar == true:
+			get_tree().change_scene_to_file("res://scenes/game.tscn")
